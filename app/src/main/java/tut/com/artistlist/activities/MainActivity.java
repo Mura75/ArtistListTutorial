89579edd109b9d4186a7eb6e7169a7a15dc94c47
Mura75 <zmurager@gmil.com>
2016-05-07 10:50:01 +0600

    Created folders.

--- app/src/main/java/tut/com/artistlist/activities/MainActivity.java
+++ app/src/main/java/tut/com/artistlist/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package tut.com.artistlist;
+package tut.com.artistlist.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import tut.com.artistlist.R;
 
 public class MainActivity extends AppCompatActivity {
 
