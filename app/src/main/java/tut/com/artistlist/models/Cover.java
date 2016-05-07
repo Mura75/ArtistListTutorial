fdb3a720456864188df6a3cb13d38ff837c0885f
Mura75 <zmurager@gmil.com>
2016-05-07 10:53:29 +0600

    Created classes: Artist and Cover.

---
+++ app/src/main/java/tut/com/artistlist/models/Cover.java
@@ -0,0 +1,35 @@
+package tut.com.artistlist.models;
+
+/**
+ * Created by Murager on 07.05.2016.
+ */
+public class Cover {
+
+    private String small;
+
+    private String big;
+
+    public String getSmall() {
+        return small;
+    }
+
+    public void setSmall(String small) {
+        this.small = small;
+    }
+
+    public String getBig() {
+        return big;
+    }
+
+    public void setBig(String big) {
+        this.big = big;
+    }
+
+    @Override
+    public String toString() {
+        return "Cover{" +
+                "small='" + small + '\'' +
+                ", big='" + big + '\'' +
+                '}';
+    }
+}
